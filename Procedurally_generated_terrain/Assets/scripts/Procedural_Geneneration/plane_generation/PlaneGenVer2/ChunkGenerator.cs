87c45116538ba39247a957dd5d97e9ac676ac716
Egecan Giray <girayegecan02@gmail.com>
2023-06-29 00:54:14 +0200

    Working but there is a problem with the location

--- Procedurally_generated_terrain/Assets/scripts/Procedural_Geneneration/plane_generation/PlaneGenVer2/ChunkGenerator.cs
+++ Procedurally_generated_terrain/Assets/scripts/Procedural_Geneneration/plane_generation/PlaneGenVer2/ChunkGenerator.cs
@@ -6,12 +6,16 @@ using UnityEngine;
 public class ChunkGenerator : MonoBehaviour
 {
     private Terrain terrain;
-    private Transform location;
+    private Transform locationTerrain;
+    private Transform locationRayCaster;
+    private RayCaster rayCaster;
     private void Start()
     {
         var parent = transform.parent;
         terrain = parent.GetComponent<Terrain>();
-        location = parent.GetComponent<Transform>();
+        locationTerrain = parent.GetComponent<Transform>();
+        locationRayCaster = GetComponentInChildren<Transform>();
+        rayCaster = GetComponentInChildren<RayCaster>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -24,43 +28,29 @@ public class ChunkGenerator : MonoBehaviour
     
     public void GenerateChunks()
     {
-        var position1 = location.position;
+        var position1 = locationTerrain.position;
         var scale = terrain.terrainData.size;
         
         Vector2 position = new Vector2(position1.x, position1.z);
-        Vector2 gridPosition = new Vector2(position1.x / scale.x, position1.z / scale.z);
-
+        Vector2 gridPosition = new Vector2(Mathf.RoundToInt(position1.x / scale.x), Mathf.RoundToInt(position1.z / scale.z));
+        int k = 0;
         for (int xOffset = -1; xOffset <= 1; xOffset++)
         {
             for (int yOffset = -1; yOffset <= 1; yOffset++)
             {
                 Vector2 neighborPos = gridPosition + new Vector2(xOffset, yOffset);
                 Vector2 worldPos = position + new Vector2(xOffset * scale.x, yOffset * scale.z);
-                
+                if (rayCaster.PositionAndRayCast(k, new Vector3(locationRayCaster.transform.position.x + scale.x/2, -100, locationRayCaster.transform.position.z  + scale.z/2), scale.x))  
+                {
+                    Debug.Log("there is a terrain on: " + neighborPos + " " + worldPos);
+                }
                 
                 
                 // Debug.Log(neighborPos + " " + worldPos);
                 // Debug.Log("next Chunk");
-                
-                
+
+                k++;
             }
         }
-        
     }
-    
-    // // Given a location, check if a chunk exists at that location
-    // public bool ChunkExistsAtLocation(Vector2Int location)
-    // {
-    //     // Calculate the position of the center of the chunk
-    //     Vector3 chunkPosition = new Vector3(location.x * chunkSize, 0f, location.y * chunkSize);
-    //
-    //     // Calculate the size of the chunk bounds
-    //     Vector3 chunkBoundsSize = new Vector3(chunkSize, chunkSize, chunkSize);
-    //
-    //     // Perform an overlap check with a BoxCollider at the chunk position
-    //     Collider[] colliders = Physics.OverlapBox(chunkPosition, chunkBoundsSize / 2f);
-    //
-    //     // Check if any colliders were found
-    //     return colliders.Length > 0;
-    // }
 }
--- Procedurally_generated_terrain/Assets/scripts/Procedural_Geneneration/plane_generation/PlaneGenVer2/RayCaster.cs
+++ Procedurally_generated_terrain/Assets/scripts/Procedural_Geneneration/plane_generation/PlaneGenVer2/RayCaster.cs
@@ -4,8 +4,44 @@ using UnityEngine;
 
 public class RayCaster : MonoBehaviour
 {
-    public bool RayCast(Ray direction, float distance)
+    public bool rayTrack;
+    public GameObject debugSphere; // Reference to the sphere prefab to instantiate
+    public bool PositionAndRayCast(int position, Vector3 origin, float size)
+{
+    switch (position)
     {
-        return Physics.Raycast(direction, distance);
+        case 0:
+            return CastRayAndDebug(origin, 225f, size, true);
+        case 1:
+            return CastRayAndDebug(origin, 180f, size, false);
+        case 2:
+            return CastRayAndDebug(origin, 135f, size, true);
+        case 3:
+            return CastRayAndDebug(origin, 270f, size, false);
+        case 5:
+            return CastRayAndDebug(origin, 90f, size, false);
+        case 6:
+            return CastRayAndDebug(origin, 315f, size, true);
+        case 7:
+            return CastRayAndDebug(origin, 0f, size, false);
+        case 8:
+            return CastRayAndDebug(origin, 45f, size, true);
+        default:
+            return false;
+    }
+}
+    private bool CastRayAndDebug(Vector3 origin, float direction, float size, bool isSide)
+    {
+        if (isSide)
+            size = Mathf.Sqrt(2) * size;
+        bool isChunk = Physics.Raycast(origin, Quaternion.AngleAxis(direction, Vector3.up) * Vector3.forward, size);
+        if(rayTrack)
+            CreateSphereAtMaxDistance(origin, Quaternion.AngleAxis(direction, Vector3.up) * Vector3.forward, size);
+        return isChunk;
+    }
+    private void CreateSphereAtMaxDistance(Vector3 origin, Vector3 direction, float distance)
+    {
+        Vector3 endPoint = origin + direction.normalized * distance;
+        Instantiate(debugSphere, endPoint, Quaternion.identity);
     }
 }
